2bb164612a6b7657f5b5ec05112a9aee552fcf6e
SHUBHTHAREJA <74976580+SHUBHTHAREJA@users.noreply.github.com>
2021-02-09 14:54:59 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -21,10 +21,15 @@ function send(){
     num1=document.getElementById("number1").value;
     num2=document.getElementById("number2").value;
 
-    question_word = "<h4 id='word_display'> Q. "+num1+num2+"</h4>"; 
+    actual_answer=parseInt(num1) * parseInt(num2);
+
+    question_number = "<h4>"+num1+"x"+num2+"</h4>"; 
     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-    row = num1+ "x" +num2;  document.getElementById("output").innerHTML = row; 
+    row = question_number+input_box+check_button;
+    
+     document.getElementById("output").innerHTML=row;
+
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 
